2f761a6322a0a45c0003b1b4d20e48a8392b50ca
jihyung2 <wlgud1116@naver.com>
2023-09-04 16:44:39 +0900

    추가 수정 많이 필요

---
+++ satellite/src/main/java/com/example/satellite/service/DataProcessingService.java
@@ -0,0 +1,13 @@
+package com.example.satellite.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DataProcessingService {
+
+    public String processReceivedData(String rawData) {
+        // rawData를 원하는 형식으로 가공하거나 처리합니다.
+        // 예: JSON 파싱, 필요한 정보 추출 등
+        return processedData;
+    }
+}
