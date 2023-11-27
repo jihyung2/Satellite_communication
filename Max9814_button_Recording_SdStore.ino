8f2c37731664e466b618d33d22a8278a3eb393b5
jihyung2 <108830942+jihyung2@users.noreply.github.com>
2023-11-27 19:00:53 +0900

    Update Max9814_button_Recording_SdStore.ino

--- Max9814_button_Recording_SdStore.ino
+++ Max9814_button_Recording_SdStore.ino
@@ -8,9 +8,11 @@ TMRpcm audio;
 bool recording_now = false;
 const int button_pin = 7;
 const int mic_pin = A0;
+const int speaker_pin = 9;
 const int sample_rate = 16000;
-
+char *filename = "myWav.wav"; 
 unsigned long button_pressed_time = 0;
+
 
 void setup() {
   Serial.begin(9600);
@@ -29,24 +31,26 @@ void setup() {
 
 void loop() {
   // 버튼이 눌렸을 때
+  audio.speakerPin = 9;
   if (digitalRead(button_pin) == HIGH && !recording_now) {
     button_pressed_time = millis();  // 버튼이 눌린 시간 기록
     recording_now = true;
-    char file_name[] = "RecordingData.wav";
-    audio.startRecording(file_name, sample_rate, mic_pin);
+    audio.startRecording(filename, sample_rate, mic_pin);
+    //audio.createWavTemplate(filename, sample_rate);
     Serial.println("녹음 중");
   }
 
   // 버튼이 눌린 후 3초 이상 경과하면 녹음 정지
   if (recording_now && millis() - button_pressed_time >= 10000) {
     recording_now = false;
-    char file_name[] = "RecordingData.wav";
-    audio.stopRecording(file_name);
+ 
+    audio.finalizeWavTemplate(filename);
     Serial.println("녹음이 저장되었습니다.");
 
     // SD 카드 확인
-    if (SD.exists(file_name)) {
+    if (SD.exists(filename)) {
       Serial.println("녹음 파일이 SD 카드에 저장되었습니다.");
+      audio.play(filename); //음성 스피커 출력
     } else {
       Serial.println("녹음 파일 저장 중 오류가 발생했습니다.");
     }
